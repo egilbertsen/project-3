ed21d3ebab1c462c82ecd7fdddccf9258e8f6f62
Michael Cheung <mcheung008@gmail.com>
2020-12-03 23:19:33 -0800

    react skeleton

---
+++ wvw-app/src/App.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+
+        <h1>
+          Willamette Valley Wineries
+        </h1>
+
+        <p>
+          The beginning of something great...
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
