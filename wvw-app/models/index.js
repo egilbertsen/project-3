079e3642cfee63b5169999498ad2fab9044dc785
JackRyan_Smith <JackRyan3d@gmail.com>
2020-12-20 22:32:46 -0800

    api

--- wvw-app/models/index.js
+++ wvw-app/models/index.js
@@ -3,3 +3,25 @@ module.exports = {
   UserList: require("./list"),
   User: require("./user")
 };
+var unirest = require("unirest");
+
+var req = unirest("POST", "https://mapboxdurationvolodimir-kudriachenkov1.p.rapidapi.com/getCyclingDuration");
+
+req.headers({
+	"content-type": "application/x-www-form-urlencoded",
+	"x-rapidapi-key": "44c27ee2d0msh7900cbe63b02c9bp125d0djsn2e1f44ff0163",
+	"x-rapidapi-host": "MapboxDurationvolodimir-kudriachenkoV1.p.rapidapi.com",
+	"useQueryString": true
+});
+
+req.form({
+	"coordinates": "undefined",
+	"accessToken": "pk.eyJ1IjoiamFja3J5YW4xMDEzIiwiYSI6ImNraW01cXYxMDBxMXoyeXFpd3RhZmkwbHAifQ.WPoQcWA-KJD44FHMsbmyNQ"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
